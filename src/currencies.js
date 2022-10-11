c4a1d32c0d1b15396e23f12cf4b9a73df2a14d57
Mustafa-Agha <mustafaagha.developer@gmail.com>
2022-10-11 14:10:48 +0200

    added firo

--- src/currencies.js
+++ src/currencies.js
@@ -182,6 +182,11 @@ var CURRENCIES = [{
     addressTypes: {prod: ['49', '7d'], testnet: ['41', '42']},
     validator: BTCValidator
 },{
+    name: 'firo',
+    symbol: 'FIRO',
+    addressTypes: {prod: ['52', '07'], testnet: ['41', 'b2']},
+    validator: BTCValidator
+},{
     name: 'neo',
     symbol: 'neo',
     addressTypes: {prod: ['17'], testnet: []},
